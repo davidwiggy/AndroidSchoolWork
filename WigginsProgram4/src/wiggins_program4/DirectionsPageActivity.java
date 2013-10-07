0590f887dcb4fd638e2852a592097c04433c93b5
David Wiggins <timothydwiggins1@gmail.com>
2013-10-07 11:19:53 -0400

    first commit

---
+++ WigginsProgram4/src/wiggins_program4/DirectionsPageActivity.java
@@ -0,0 +1,40 @@
+//FILE : DirectionsPageActivity.java
+//PROG : Dave Jones
+//PURP : Shows "page of directions" in a ScrollView
+package wiggins_program4;
+
+import edu.jones.demogamestartarrayadaptor.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DirectionsPageActivity extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.directions);
+        
+        setDoneButtonListener();
+    }//END onCreate
+
+	//Set up the Done button to initialize intent and finish
+		private void setDoneButtonListener()
+		{
+			Button doneButton = (Button) findViewById(R.id.doneBTN);
+			doneButton.setOnClickListener
+			(
+				new View.OnClickListener()
+				{
+					@Override
+					public void onClick(View v)
+					{
+						finish();
+					}
+				}
+			);//END setOnClickListener
+		}//END setDoneButtonListener
+		
+}//END DirectionsPageActivity
